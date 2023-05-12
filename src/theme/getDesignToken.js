8e426d0f1d6dbbaebe228aef5178025a27f6baa9
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-12 16:25:07 +0300

    finish

--- src/theme/getDesignToken.js
+++ src/theme/getDesignToken.js
@@ -5,36 +5,24 @@ export const getDesignTokens = mode => ({
       ? {
           // palette values for light mode
           primary: {
-            main: '#455a64',
+            main: '#37474f',
           },
           secondary: {
-            main: '#447cbb',
-            light: '#cfd8dc',
+            main: '#f57f17',
           },
           background: {
-            default: '#b0bec5',
-            paper: '#e0e0e0',
-          },
-          text: {
-            primary: 'rgba(90,88,88,0.87)',
-            secondary: 'rgba(4,4,4,0.6)',
-            disabled: 'rgba(158,156,156,0.38)',
+            paper: '#cfd8dc',
           },
         }
       : {
           primary: {
-            main: '#3f51b5',
+            main: '#eceff1',
           },
           secondary: {
-            main: '#f50057',
-          },
-          text: {
-            primary: '#e2dcdc',
-            secondary: 'rgba(206,200,200,0.7)',
+            main: '#f57f17',
           },
           background: {
-            default: '#031841',
-            paper: '#0e0e3b',
+            paper: '#263238',
           },
         }),
   },
