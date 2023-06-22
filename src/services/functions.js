7e6422338f9c47e2b7f7e4ecf2690174b72ecd51
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-06-22 19:46:55 +0300

    save

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,7 +1,16 @@
-export const createObj = ({ id, name, phone, newName, newPhone }) => {
+export const createObj = ({
+  id,
+  name,
+  phone,
+  email,
+  newName,
+  newPhone,
+  newEmail,
+}) => {
   const obj = {};
   obj.id = id;
   if (name !== newName && newName) obj.name = newName;
   if (phone !== newPhone && newPhone) obj.phone = newPhone;
+  if (email !== newEmail && newEmail) obj.email = newEmail;
   return obj;
 };
