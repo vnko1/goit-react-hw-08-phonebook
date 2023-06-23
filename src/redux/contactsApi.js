f5973425cb54bb65d363f65a39517faf994cd390
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-06-23 11:26:43 +0300

    save

--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -18,37 +18,32 @@ export const contactsApi = createApi({
   endpoints: build => ({
     fetchContacts: build.query({
       query: () => '/contacts',
-      providesTags: result => {
-        return result
+      providesTags: result =>
+        result.contacts
           ? [
-              ...result.contacts.map(({ id }) => ({ type: 'Contacts', id })),
+              ...result.contacts.map(({ _id: id }) => ({
+                type: 'Contacts',
+                id,
+              })),
               { type: 'Contacts', id: 'LIST' },
             ]
-          : [{ type: 'Contacts', id: 'LIST' }];
-      },
+          : [{ type: 'Contacts', id: 'LIST' }],
     }),
     addContact: build.mutation({
-      query: contact => {
-        return { url: '/contacts', method: 'POST', body: contact };
-      },
+      query: contact => ({ url: '/contacts', method: 'POST', body: contact }),
       invalidatesTags: [{ type: 'Contacts', id: 'LIST' }],
     }),
     editContact: build.mutation({
-      query: ({ id, name, phone, email }) => {
-        return {
-          url: `/contacts/${id}`,
-          method: 'PUT',
-          body: { name, phone, email },
-        };
-      },
+      query: ({ id, name, phone, email }) => ({
+        url: `/contacts/${id}`,
+        method: 'PUT',
+        body: { name, phone, email },
+      }),
       invalidatesTags: [{ type: 'Contacts', id: 'LIST' }],
     }),
     deleteContact: build.mutation({
       query: id => ({ url: `/contacts/${id}`, method: 'DELETE' }),
-      invalidatesTags: (result, error, id) => {
-        console.log(id);
-        return [{ type: 'Contacts', id }];
-      },
+      invalidatesTags: (result, error, id) => [{ type: 'Contacts', id }],
     }),
   }),
 });
